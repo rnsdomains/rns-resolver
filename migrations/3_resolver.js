014f313b4fc1cacdfbbb19a5a45eaeeb2d65cba7
Ilan Olkies <ilanolkies@outlook.com>
2020-05-07 06:23:24 -0300

    Fix test migrations

--- migrations/3_resolver.js
+++ migrations/3_resolver.js
@@ -10,46 +10,48 @@ const { encodeCall } = require('@openzeppelin/upgrades');
 const RNS_MULTI_SIG = '0x39e00d2616e792f50ddd33bbe46e8bf55eadebee';
 
 module.exports = (deployer, network, accounts) => {
-  deployer.then(async () => {
-    if (network === 'develop') {
-      const rns = await deployer.deploy(RNS);
-      this.rnsAddress = rns.address;
-    } else if (network === 'testnet') {
-      this.rnsAddress = '0x7d284aaac6e925aad802a53c0c69efe3764597b8';
-    } else if (network === 'mainnet') {
-      this.rnsAddress = '0xcb868aeabd31e2b66f74e9a55cf064abb31a4ad5';
-    }
+  if(network !== 'test') {
+    deployer.then(async () => {
+      if (network === 'develop') {
+        const rns = await deployer.deploy(RNS);
+        this.rnsAddress = rns.address;
+      } else if (network === 'testnet') {
+        this.rnsAddress = '0x7d284aaac6e925aad802a53c0c69efe3764597b8';
+      } else if (network === 'mainnet') {
+        this.rnsAddress = '0xcb868aeabd31e2b66f74e9a55cf064abb31a4ad5';
+      }
 
-    const proxyFactory = await deployer.deploy(ProxyFactory);
-    const proxyAdmin = await deployer.deploy(ProxyAdmin);
-    const resolverV1 = await deployer.deploy(ResolverV1);
+      const proxyFactory = await deployer.deploy(ProxyFactory);
+      const proxyAdmin = await deployer.deploy(ProxyAdmin);
+      const resolverV1 = await deployer.deploy(ResolverV1);
 
-    const salt = '16';
-    const data = encodeCall('initialize', ['address'], [this.rnsAddress]);
-    await proxyFactory.deploy(salt, resolverV1.address, proxyAdmin.address, data);
+      const salt = '16';
+      const data = encodeCall('initialize', ['address'], [this.rnsAddress]);
+      await proxyFactory.deploy(salt, resolverV1.address, proxyAdmin.address, data);
 
-    if (network === 'develop') {
-      await proxyAdmin.transferOwnership(accounts[0]);
-    } else if (network === 'mainnet') {
-      await proxyAdmin.transferOwnership(RNS_MULTI_SIG);
-    }
+      if (network === 'develop') {
+        await proxyAdmin.transferOwnership(accounts[0]);
+      } else if (network === 'mainnet') {
+        await proxyAdmin.transferOwnership(RNS_MULTI_SIG);
+      }
 
-    console.log('Proxy factory: ' + proxyFactory.address);
-    console.log('Proxy admin: ' + proxyAdmin.address);
-    console.log('Resolver implementation: ' + resolverV1.address);
-    console.log('-------------');
-    const deploymentAddress = await proxyFactory.getDeploymentAddress(salt, accounts[0]);
-    console.log('Resulting proxy deployment address: ' + deploymentAddress);
-    const implementationAddress = await proxyAdmin.getProxyImplementation(deploymentAddress);
-    console.log('Resulting querying implementation address: ' + implementationAddress);
+      console.log('Proxy factory: ' + proxyFactory.address);
+      console.log('Proxy admin: ' + proxyAdmin.address);
+      console.log('Resolver implementation: ' + resolverV1.address);
+      console.log('-------------');
+      const deploymentAddress = await proxyFactory.getDeploymentAddress(salt, accounts[0]);
+      console.log('Resulting proxy deployment address: ' + deploymentAddress);
+      const implementationAddress = await proxyAdmin.getProxyImplementation(deploymentAddress);
+      console.log('Resulting querying implementation address: ' + implementationAddress);
 
 
-    assert.equal(implementationAddress, resolverV1.address);
-    const owner = await proxyAdmin.owner();
-    if (network === 'mainnet') {
-      assert.equal(owner, RNS_MULTI_SIG);
-    } else {
-      assert.equal(owner, accounts[0]);
-    }
-  });
+      assert.equal(implementationAddress, resolverV1.address);
+      const owner = await proxyAdmin.owner();
+      if (network === 'mainnet') {
+        assert.equal(owner, RNS_MULTI_SIG);
+      } else {
+        assert.equal(owner, accounts[0]);
+      }
+    });
+  }
 };
